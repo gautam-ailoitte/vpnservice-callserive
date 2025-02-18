74fe6118c50aff0248b8eea07eb9c496e9a88521
gautam-ailoitte <gautam.kumar@ailoitte.com>
2025-02-18 17:25:13 +0530

    rings in silent crashes in dnd, we can play sound then stop on call disconnect or pick up

--- app/src/main/kotlin/com/example/vpnservices/MyCallManagerService.kt
+++ app/src/main/kotlin/com/example/vpnservices/MyCallManagerService.kt
@@ -1,11 +1,14 @@
 package com.example.vpnservices
 
 
+import android.media.AudioManager
 import android.os.Build
 import android.telecom.CallScreeningService
 import android.telecom.Call.Details
 import android.util.Log
 import androidx.annotation.RequiresApi
+
+
 
 @RequiresApi(Build.VERSION_CODES.N)
 class MyCallManagerService : CallScreeningService() {
@@ -17,6 +20,22 @@ class MyCallManagerService : CallScreeningService() {
 
         // For demonstration purposes, let's block all calls except from a specific number
         if (incomingNumber == "+917542036307") {
+
+            // ringing
+            val audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                // For Android Pie and above, check if DND mode is enabled and override it
+                if (audioManager.ringerMode == AudioManager.RINGER_MODE_SILENT ||
+                    audioManager.ringerMode == AudioManager.RINGER_MODE_VIBRATE) {
+                    audioManager.ringerMode = AudioManager.RINGER_MODE_NORMAL // Force normal mode
+                }
+            } else {
+                // For older versions, set volume directly
+                audioManager.ringerMode = AudioManager.RINGER_MODE_NORMAL
+                audioManager.setStreamVolume(AudioManager.STREAM_RING,
+                    audioManager.getStreamMaxVolume(AudioManager.STREAM_RING), 0)
+            }
             // Allow call
             val response = CallResponse.Builder()
                 .setDisallowCall(false)  // Allow call
